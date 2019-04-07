8f84cd07ad142a7b3df60fca94f74f0e1725c0a2
sinha414tanya <49367140+sinha414tanya@users.noreply.github.com>
2019-04-07 21:11:13 +0530

    Update max_ele.py

--- max_ele.py
+++ max_ele.py
@@ -2,8 +2,9 @@ N=int(input())
 array=list(map(int,input().split()))
 
 def display_sum(N,array):
-    if(1>=N<=100000):
+    if(1<=N<=100000):
         array.sort(reverse=True)
         print(array[0])
+
 
 display_sum(N,array)
