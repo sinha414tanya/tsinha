f150f700fc439567867ac3361f9d16d716e99ccb
sinha414tanya <49367140+sinha414tanya@users.noreply.github.com>
2019-04-07 21:41:44 +0530

    Update median_ele.py

--- median_ele.py
+++ median_ele.py
@@ -3,11 +3,6 @@ array=list(map(int,input().split()))
 def median(n,array):
     if(1<=n<=1000):
         array.sort()
-        if(n%2==0):
-            a=int(n/2)
-            print(array[a-1])
-        else:
-            a=int(n/2)
-            print(array[a])
-
+        a=int(n/2)
+        print(array[a])
 median(n,array)
