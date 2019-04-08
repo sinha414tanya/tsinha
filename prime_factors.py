cff1e50add3948069a65779562d1e5fe812f9cc3
sinha414tanya <49367140+sinha414tanya@users.noreply.github.com>
2019-04-08 22:39:33 +0530

    Add files via upload

---
+++ prime_factors.py
@@ -0,0 +1,25 @@
+n=int(input("Enter an integer:"))
+a=[]
+for i in range(2,n+1):
+    if n%i==0:
+        a.append(i)
+def check(num):
+    c=0
+
+    for i in range(2,num):
+        if(num%i==0):
+            c=c+1
+    if c==0:
+        return True
+    else:
+        return False
+b=[]
+for i in a:
+    if(check(i)==True):
+        b.append(i)
+print(*b)
+
+
+
+
+
