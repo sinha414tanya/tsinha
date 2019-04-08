6cf725b9f17a2130ffcd8930cd82ddd7bedf1c49
sinha414tanya <49367140+sinha414tanya@users.noreply.github.com>
2019-04-08 18:03:56 +0530

    Add files via upload

---
+++ week.py
@@ -0,0 +1,23 @@
+choice=str(input())
+def display(choice):
+    if choice=="Monday":
+        print("no")
+    elif choice == "Tuesday":
+        print("no")
+    elif choice == "Wednesday":
+        print("no")
+    elif choice == "Thursday":
+        print("no")
+    elif choice == "Friday":
+        print("no")
+    elif choice=="Saturday":
+        print("yes")
+    elif choice == "Sunday":
+        print("yes")
+    else:
+        print("invalid input")
+
+
+
+display(choice)
+
