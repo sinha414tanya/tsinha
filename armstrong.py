50623cefd3d5fa0b35b6953980a35a9e6f953313
Tanya Chitranshi <49367140+sinha414tanya@users.noreply.github.com>
2019-04-13 00:06:13 +0530

    Armstrong number

--- armstrong.py
+++ armstrong.py
@@ -1,18 +1,19 @@
 num1=int(input())
 
 def check_armstrong(num1):
-    num = num1
-    st=str(num1)
-    l=len(st)
-    sum=0
-    while(num>0):
-        c=num%10
-        sum=sum+(c**l)
-        num=num//10
-    if int(num1)==sum:
-        return True
-    else:
-        return False
+    if num1<=100000:
+        num = num1
+        st=str(num1)
+        l=len(st)
+        sum=0
+        while(num>0):
+            c=num%10
+            sum=sum+(c**l)
+            num=num//10
+        if int(num1)==sum:
+            return True
+        else:
+            return False
 if check_armstrong(num1)==True:
     print("yes")
 else:
